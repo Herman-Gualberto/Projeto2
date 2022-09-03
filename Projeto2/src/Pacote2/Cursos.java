2e5d03ea64d4395206eda7f5eae8b2f5c65942a7
LVMU2C631 <LVMU2C631@IBM-PE07R44E>
2022-09-02 21:02:36 -0300

    cursos

---
+++ Projeto2/src/Pacote2/Cursos.java
@@ -0,0 +1,10 @@
+package Pacote2;
+
+public class Cursos {
+	public static void main(String[] args) {
+		System.out.println("Direito");
+		System.out.println("Engenharia");
+		System.out.println("Pedagogia");
+	}
+
+}
