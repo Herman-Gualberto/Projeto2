fc85de42e6c8dc8587bd228160b389a079ad84dc
LVMU2C631 <LVMU2C631@IBM-PE07R44E>
2022-09-08 14:06:55 -0300

    calculando a media

--- Projeto2/src/Pacote2/Cursos.java
+++ Projeto2/src/Pacote2/Cursos.java
@@ -76,7 +76,20 @@ public class Cursos {
 		}else {
 			System.out.println("Cancelou a pergunta");
 		}	
+		
+		/*=======================================================*/
+		/* Cálculo de Média */
+		String nota1 = JOptionPane.showInputDialog("Informe a nota 1");
+		String nota2 = JOptionPane.showInputDialog("Informe a nota 2");
+		String nota3 = JOptionPane.showInputDialog("Informe a nota 3");
+		String nota4 = JOptionPane.showInputDialog("Informe a nota 4");
+		
+		double dNota1 = Double.parseDouble(nota1);
+		double dNota2 = Double.parseDouble(nota2);
+		double dNota3 = Double.parseDouble(nota3);
+		double dNota4 = Double.parseDouble(nota4);
+		
+		double media = (dNota1 + dNota2 + dNota3 + dNota4)/4;
+		JOptionPane.showMessageDialog(null,"A média é " + media);
 	}
-	
-
 }
