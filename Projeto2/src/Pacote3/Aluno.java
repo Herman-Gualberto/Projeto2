08a9ebff6ca3f0280be4cc33f64971b80f6fbf93
LVMU2C631 <LVMU2C631@IBM-PE07R44E>
2022-09-09 08:02:52 -0300

    objetos

--- Projeto2/src/Pacote3/Aluno.java
+++ Projeto2/src/Pacote3/Aluno.java
@@ -1,16 +1,18 @@
 package Pacote3;
 /*Esta é a no classe/objeto que representa o Aluno*/
 public class Aluno {
-	String nome;
-	int idade;
-	String dataNascimento;
-	String registroGeral;
-	String numeroCpf;
-	String nomeMae;
-	String nomePai;
-	String dataMatricula;
-	String nomeEscola;
-	String serieMatriculado;
+	
+	/*Esses são os atributos do aluno*/
+	public String nome;
+	public int idade;
+	public String dataNascimento;
+	public String registroGeral;
+	public String numeroCpf;
+	public String nomeMae;
+	public String nomePai;
+	public String dataMatricula;
+	public String nomeEscola;
+	public String serieMatriculado;
 
 	public Aluno() { /*Cria os dados na memoria - Sendo padrao do java*/
 		
