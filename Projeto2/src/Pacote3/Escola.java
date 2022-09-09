4099f022fd46f1c8847ee831f8d6ffb12a64d915
LVMU2C631 <LVMU2C631@IBM-PE07R44E>
2022-09-09 08:03:15 -0300

    chamada de objetos

--- Projeto2/src/Pacote3/Escola.java
+++ Projeto2/src/Pacote3/Escola.java
@@ -4,10 +4,15 @@ public class Escola {
 
 	public static void main(String[] args) {
 		
+		/*new Aluno() é uma instancia (Criação de objeto)*/
+		/*aluno1 é uma referência para o objeto aluno*/
+		
 		Aluno aluno1 = new Aluno(); /*Aqui será o João*/
+		aluno1.nome = "João";
+		System.out.println("Nome do aluno 1 é " + aluno1.nome);
+		
 		Aluno aluno2 = new Aluno(); /*Aqui será o Pedro*/
 		Aluno aluno3 = new Aluno(); /*Aqui será o Alex*/
-		
 		Aluno aluno4 = new Aluno("Maria");
 		Aluno aluno5 = new Aluno("José", 50);
 	}
